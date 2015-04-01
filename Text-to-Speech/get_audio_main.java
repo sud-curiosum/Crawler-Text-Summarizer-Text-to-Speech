468f5675f8f6b495c9c8a133b49c10b94c194999
Sudhanshu Singh <sudhanshusingh3@hotmail.com>
2015-04-02 02:30:24 +0530

    Create get_audio_main.java

---
+++ Text-to-Speech/get_audio_main.java
@@ -0,0 +1,21 @@
+package GOOGLE_SPEECH;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class GETAUDIO {    
+	
+    public static void main(String[] args) throws Exception {
+  
+        BufferedReader read1 = new BufferedReader(new FileReader("read.txt"));
+        String temp;
+        int index = 0;
+        while((temp = read1.readLine()) != null)
+        {
+        	//new thread for the new text
+        	new create_audio(index, temp);                               
+            index++;
+        }
+       read1.close();
+    }
+    
+}
